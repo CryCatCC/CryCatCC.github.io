6dcc05f8949ce115005025071fb95f62c33254d2
CryCatCC <160016979+CryCatCC@users.noreply.github.com>
2024-03-06 11:53:16 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,7 +1,7 @@
 console.log("Щоб розпочати, уведіть таку фінкцію: triangle(значення1, 'тип1', значення2, 'тип2')");
 console.log("Типи, які ви можете задати:  ");
 console.log("• leg - катет");
-console.log("• leghypotenuse - гіпотенуза");
+console.log("• hypotenuse - гіпотенуза");
 console.log("• adjacent angle - прилеглий до катета кут");
 console.log("• opposite angle - протилежний до катета кут");
 console.log("• angle - один з двох гострих кутів");
@@ -73,8 +73,17 @@ function triangle(arg1, type1, arg2, type2) {
         return "failed";
     }
 
-    if (arg1 <= 0 || arg2 <= 0) {
-        console.log("Значення аргументів не можуть бути від'ємними або нульовими.");
+    // if (arg1 <= 0 || arg2 <= 0) {
+    //     console.log("Значення аргументів не можуть бути від'ємними або нульовими.");
+    //     return "failed";
+    // }
+    // if ( alpha>=90 || alpha <=0){
+    //     console.log("Некоректні значення кутів (кути мають бути вiд 0 до 90 градусiв не включно).");
+    //     return "failed";
+    // }
+
+    if (arg1 < 1 || arg2 < 1) {
+        console.log("Значення аргументів не можуть бути від'ємними, нульовими, або у степені е-n.");
         return "failed";
     }
     if ( alpha>=90 || alpha <=0){
@@ -82,12 +91,21 @@ function triangle(arg1, type1, arg2, type2) {
         return "failed";
     }
 
+
     if (a && b) {
+        if (a >= c || b >= c) {
+            console.log("Один з катетів не може бути більшим або рівним гіпотенузі.");
+            return "failed";
+        }
         c = Math.sqrt(a*a+b*b);
         alpha = Math.acos((b*b+c*c-a*a)/(2*b*c)) * 180 / Math.PI;
         beta = 90 - alpha;
     }
     else if (a && c) {
+        if (a >= c) {
+            console.log("Катет не може бути більшим або рівним гіпотенузі.");
+            return "failed";
+        }
         b = Math.sqrt(c*c-a*a);
         alpha = Math.acos((b*b+c*c-a*a)/(2*b*c)) * 180 / Math.PI;
         beta = 90 - alpha;
@@ -95,11 +113,24 @@ function triangle(arg1, type1, arg2, type2) {
     else if (a && alpha){
         b = a*Math.tan(beta * Math.PI / 180); ;
         c = Math.sqrt(a*a+b*b);
+        if (a >= c) {
+            console.log("Катет не може бути більшим або рівним гіпотенузі.");
+            return "failed";
+        }
     }
     else if (alpha && c){
         b = c*Math.cos(alpha * Math.PI / 180);
         a = c*Math.sin(alpha* Math.PI / 180);
+        if (a >= c) {
+            console.log("Катет не може бути більшим або рівним гіпотенузі.");
+            return "failed";
+        }
     }
+
+    // if (a === 0 || b === 0 || c === 0) {
+    //     console.log("Утворений трикутник не може мати катети або кути, що дорівнюють нулю.");
+    //     return "failed";
+    // }
 
     console.log(`Катет a: ${a.toFixed(3)}`);  
     console.log(`Катет b: ${b.toFixed(3)}`);
@@ -107,43 +138,11 @@ function triangle(arg1, type1, arg2, type2) {
     console.log(`Гострий кут alpha: ${alpha.toFixed(3)} градуси`);
     console.log(`Гострий кут beta: ${beta.toFixed(3)} градуси`);
 
+    // console.log(`Катет a: ${a}`);  
+    // console.log(`Катет b: ${b}`);
+    // console.log(`Гіпотенуза (c): ${c}`);
+    // console.log(`Гострий кут alpha: ${alpha.toFixed(10)} градуси`);
+    // console.log(`Гострий кут beta: ${beta.toFixed(10)} градуси`);
+
     return "success";
 }
-
-// triangle(5,"leg",4,"leg");
-
-// triangle(10,"hypotenuse",5,"leg");
-// triangle(4,"leg",5,"hypotenuse");
-
-// console.log("--------------------------------------");
-// console.log("--------------------------------------");
-
-// triangle(4,"leg",60,"opposite angle");
-// triangle(60,"opposite angle",4,"leg");
-// triangle(4,"leg",60,"adjacent angle");
-// triangle(60,"adjacent angle",4,"leg");
-
-
-// console.log("--------------------------------------");
-// console.log("--------------------------------------");
-// console.log("--------------------------------------");
-
-
-// triangle(4,"hypotenuse",60,"opposite angle");
-// triangle(60,"opposite angle",4,"hypotenuse");
-
-// triangle(4,"hypotenuse",60,"adjacent angle");
-// triangle(60,"adjacent angle",4,"hypotenuse");
-
-// triangle(10,"hypotenuse",60,"angle");
-// console.log(128);
-// triangle(60,"angle",10,"hypotenuse");
-
-// console.log("---------неправильний набір аргументів--------");
-// triangle(10,"hypotenuse",5,"hypotenuse");
-// triangle(10,"angle",5,"angle");
-// triangle(60,"adjacent angle",60,"angle");
-// console.log("---------неправильне значення аргументів--------");
-// triangle(-10,"hyotenuse",5,"angle");
-// triangle(90,"angle",5,"leg");
-// triangle(60,"leg",-13,"angle");
